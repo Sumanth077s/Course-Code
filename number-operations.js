32f2eb807830bb8a40e3183533a7dc4888449320
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-02 23:35:05 +0530

    number operator

---
+++ number-operations.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var costOfMilkShake = 350;
+var discount = 0.5;
+var tax = 18;
+var quantity = 3;
+
+var total = (costOfMilkShake + tax - discount) * 1;
+console.log('The Total payable amount is - ' + total);
+
+var numOfEpisodes = 22;
+var perSeason = 22/2;
+
+console.log('Episode per season: ' + perSeason);
+
+console.log(2 ** 6 + 'bits'); /* The result is 2 power 6 (2^6) */ 
+console.log(20 % 6);
