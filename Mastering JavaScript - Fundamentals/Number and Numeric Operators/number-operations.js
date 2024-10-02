a0806c44296ef4ddb616d18bdcf5f1a3e0e24d3c
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-02 23:30:30 +0530

    number operations in JavaScript

--- Mastering JavaScript - Fundamentals/Number and Numeric Operators/number-operations.js
+++ Mastering JavaScript - Fundamentals/Number and Numeric Operators/number-operations.js
@@ -1,17 +1,3 @@
-// index.js
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello, World!\n');
-// });
-
-// server.listen(3000, () => {
-//     console.log('Server running at http://127.0.0.1:3000/');
-// });
-
-
 'use strict';
 
 var costOfMilkShake = 350;
