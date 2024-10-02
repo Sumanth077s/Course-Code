dbdb9fb5f737f24eeafebda9659e7e78d233d32a
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-02 23:13:12 +0530

    JavaScript addon

--- Mastering JavaScript - Fundamentals/test-file.js
+++ Mastering JavaScript - Fundamentals/test-file.js
@@ -1,1 +1,24 @@
-console.log("Hello world") 
+// index.js
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello, World!\n');
+// });
+
+// server.listen(3000, () => {
+//     console.log('Server running at http://127.0.0.1:3000/');
+// });
+
+
+'use strict';
+
+var temp = 12;
+console.log("The outside temperature is " + temp);
+
+var salary = 98;
+var salaryInWords = nintyeight;
+
+console.log(typeof salary);
+console.log(typeof salaryInWords);
