2ea3edfc525a8f135ab43066104d527eaae5a42d
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-09 00:48:24 +0530

    In-Equality Operator

---
+++ Mastering JavaScript - Fundamentals/The Abbsolute Fundamentals/InEquality-Operator.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//  In-Equality Operator
+
+console.log('Inequality Operator');
+
+console.log(10 != 20);
+console.log('20' != 20);
+console.log(null != undefined);
+
+console.log('Strict Inequality or Nonidentity operator');
+console.log(10 !== 20);
+console.log('20' !== 20);
+console.log(null !== undefined);
