d3bd12d59e4037b1836ca0f5bfee68229273f3de
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-08 22:51:02 +0530

    temp file for code testing

---
+++ Mastering JavaScript - Fundamentals/The Abbsolute Fundamentals/temp.js
@@ -0,0 +1,17 @@
+'use strict';
+// Write your code here...
+
+var user = {
+    name: 'Michelle Roberts',
+    age: 28,
+    location: {
+        city: 'New York',
+        country: 'US',
+    },
+    isAdmin: false,
+};
+
+console.log('User: ' + user.name);
+console.log('City: ' + user.location.city);
+console.log('Age: ' + user.age);
+console.log('Is User an Administrator?' + user.isAdmin);
