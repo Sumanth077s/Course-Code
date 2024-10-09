a7ff5950d99c12e13c57f7249d4bebf33368d740
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-09 22:57:25 +0530

    Exception Error handling

---
+++ Mastering JavaScript - Fundamentals/The Abbsolute Fundamentals/Exception-Handling-Error.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// var a = 10;
+var a = '10';
+// var b = 20n;
+var b = 20;
+
+// var c = a + b;
+
+// console.log(`The result of the sum of ${c}`);
+
+try {
+    if(typeof a != typeof b) 
+        throw new Error(`Both operands must belongs to the same data type!`); /* Custom Error */
+    var c = a + b;
+    console.log(`The result of the sum is ${c}`);
+} catch (Error) {
+    // console.log(`Oops! There was an error!`);
+    console.log(Error);  /* We want to know what is the error so it goes to the "throw new Error" and show the error message */
+} finally {
+    console.log(`I will run no matter what!`);
+}
+
+var pi = Math.PI;
+console.log(`The value of PI is ${pi}`);
