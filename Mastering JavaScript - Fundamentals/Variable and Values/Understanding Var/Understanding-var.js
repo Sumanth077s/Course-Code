3ae3798339f8911df207280d46a9338bced6b977
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-10 19:19:57 +0530

    Understanding Var

---
+++ Mastering JavaScript - Fundamentals/Variable and Values/Understanding Var/Understanding-var.js
@@ -0,0 +1,27 @@
+// 'use strict'
+
+var user = 'Sarah Rice';
+
+// console.log(globalThis.hasOwnProperty('user'));
+// console.log(globalThis);
+
+function sayHi() {
+    var greeting = "It's so good to meet you!";
+    
+    function greet(){
+        return `She also says, ${greeting}`;
+    }
+    console.log(`${user} says Hi!`);
+    console.log(greet());
+}
+
+sayHi();
+// console.log(greeting);
+
+// try {
+//     console.log(greeting);
+// } catch (error) {
+//     console.log(`Throw Error: This is not access because it is in the function not called outside the function`);
+// } finally {
+//     console.log('Execution complete');
+// }
