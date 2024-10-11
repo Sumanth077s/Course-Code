dd058fe809eef33f3e8c75771706d203e14b5304
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-11 23:55:34 +0530

    Ternary Operator: A shorthand way to write an if-else statement.

---
+++ Mastering JavaScript - Fundamentals/Ternary Operator/Ternary-Operator.js
@@ -0,0 +1,10 @@
+// Ternary Operator: A shorthand way to write an if-else statement.
+
+'use strict';
+
+let temperature = 35;
+
+// condition ? value_if_true : value_if_false;
+let feelTemp = temperature >= 25 ? 'warm' : 'cool';
+
+console.log(`Today is a ${feelTemp} day`);
