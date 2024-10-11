d77be02311791316d20544dd454841074a9c96b7
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-12 00:12:24 +0530

    Ternary Operator: A shorthand way to write an if-else statement.

--- Mastering JavaScript - Fundamentals/Ternary Operator/Ternary-Operator.js
+++ Mastering JavaScript - Fundamentals/Ternary Operator/Ternary-Operator.js
@@ -10,7 +10,7 @@ let feelTemp = temperature >= 25 ? 'warm' : 'cool';
 console.log(`Today is a ${feelTemp} day`);
 
 
-let cost = 2100;
+let cost = 5000;
 
 let discount = cost < 1000 ? 0 : 10;
 
@@ -18,3 +18,19 @@ let totalCost = cost - cost * (discount / 100);
 
 console.log(`Original CostL $${cost}`);
 console.log(`Original CostL $${totalCost}`);
+
+
+let today = new Date().getDay();
+
+let day = today === 0 ? 'Sunday' : 
+            today === 1 ? 'Monday' : 
+            today === 2 ? 'Tuesday' : 
+            today === 3 ? 'Wednesday' : 
+            today === 4 ? 'Thursday' : 
+            today === 5 ? 'Friday' : 
+            today === 6 ? 'Saturday' : 
+            'Not exactly a real day...' ;
+
+console.log(`Today is ${day}`);
+
+console.log(today); /* answer is 6 = Friday */
