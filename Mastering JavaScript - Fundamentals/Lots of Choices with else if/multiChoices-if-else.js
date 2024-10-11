a86974001480bd79f048e5bfb8bc2d25436dfe15
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-11 20:59:58 +0530

    if else, else if conditions

--- Mastering JavaScript - Fundamentals/Lots of Choices with else if/multiChoices-if-else.js
+++ Mastering JavaScript - Fundamentals/Lots of Choices with else if/multiChoices-if-else.js
@@ -4,6 +4,10 @@ let currentHour = new Date().getHours();
 
 if(currentHour >= 0 && currentHour <= 12) {
     console.log('Good Morning!');
+} else if(currentHour >= 12 && currentHour <= 16) {
+    console.log('Good Afternoon!');
+} else if(currentHour >= 16 && currentHour <= 20) {
+    console.log('Good Evening');
 } else {
-    console.log('Good Day!');
+    console.log('Good Night');
 }
