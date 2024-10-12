d22ebf310c70a0e9cfe3a183eec8a8ae3f7142e2
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-12 18:17:37 +0530

    Binary Logial Operator Folder

--- Mastering JavaScript - Fundamentals/test-file.js
+++ Mastering JavaScript - Fundamentals/test-file.js
@@ -1,30 +1,9 @@
-// index.js
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello, World!\n');
-// });
-
-// server.listen(3000, () => {
-//     console.log('Server running at http://127.0.0.1:3000/');
-// });
-
-
 'use strict';
 
-var costOfMilkShake = 350;
-var discount = 0.5;
-var tax = 18;
-var quantity = 3;
-
-var total = (costOfMilkShake + tax - discount) * 1;
-console.log('The Total payable amount is - ' + total);
-
-var numOfEpisodes = 22;
-var perSeason = 22/2;
-
-console.log('Episode per season: ' + perSeason);
-
-console.log(2 ** 6 + 'bits'); /* The result is 2 power 6 (2^6) */ 
+const number = 5;
+const result = number * 2 > 5 
+    ? number === 10 
+        ? 'Number is 5' 
+        : 'Twice of number is not 10' 
+    : 'Number is not 5';
+console.log(result);
