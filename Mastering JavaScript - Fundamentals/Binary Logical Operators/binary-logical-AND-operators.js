2a5d34c33eeea375bcabb992d88be7e30d910db7
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-12 19:23:05 +0530

    AND, AND-OR Assignment Operator

--- Mastering JavaScript - Fundamentals/Binary Logical Operators/binary-logical-AND-operators.js
+++ Mastering JavaScript - Fundamentals/Binary Logical Operators/binary-logical-AND-operators.js
@@ -5,4 +5,28 @@
 // let isUserSmiling = true;
 let isUserSmiling = false;
 
+// The && operator only moves to the second part of the expression if the first part is true.
 isUserSmiling && console.log('User is smiling');
+
+
+// Logial AND Assignment Operator 
+
+/*
+let isSunny = true; // The box has 'true'
+let weather = 'sunny';
+
+isSunny &&= weather; // Since isSunny is true, it changes isSunny to 'sunny'
+
+console.log(isSunny); // Output: 'sunny' 
+*/
+
+let dataDownloaded = 95;
+// let dataDownloaded = 100; 
+
+let hasBeenDownloaded = dataDownloaded === 100;
+
+hasBeenDownloaded &&= 'Data download now complete...';
+
+hasBeenDownloaded ||= 'Downloading...';
+
+console.log(hasBeenDownloaded);
