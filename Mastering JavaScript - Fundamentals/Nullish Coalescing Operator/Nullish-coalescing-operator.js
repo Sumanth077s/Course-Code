b87a26e6bcf3c95654f04c494c450f0220b71f7c
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-12 23:03:58 +0530

    Nullish coalescing Operator ??

--- Mastering JavaScript - Fundamentals/Nullish Coalescing Operator/Nullish-coalescing-operator.js
+++ Mastering JavaScript - Fundamentals/Nullish Coalescing Operator/Nullish-coalescing-operator.js
@@ -24,4 +24,23 @@ console.log('The 2nd product');
 console.log(`Product: ${products[1].name || 'N/A'}`);
 console.log(`Cost: ${products[1].cost || 'N/A'}`);
 // console.log(`Shipping: ${products[1].shipping || 2.5}`);
-console.log(`Shipping: ${products[1].shipping ?? 2.5}`);
+console.log(`Shipping: ${products[1].shipping ?? 2.5}`); /* Use "??" to check for null or undefined shipping, otherwise display 0 or provided value */
+
+
+let tempData = [
+    {
+        location: 'Block A - East',
+        sensor1: 0,
+        sensor2: 0,
+        sensor3: 4.2,
+    },
+    {
+        location: 'Block B - East',
+        sensor1: 0,
+        sensor2: 4,
+    }
+];
+
+console.log(`${tempData[0].location}: ${tempData[0].sensor1 || tempData[0].sensor2 || tempData[0].sensor3} Degree`);
+
+console.log(`${tempData[1].location}: ${tempData[1].sensor1 ?? tempData[1].sensor2} Degree`); /* ?? it will show the 0 too */
