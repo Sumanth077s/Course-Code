9429371851ab0a971b566be9e918abf10c57fe8a
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-13 23:21:04 +0530

    function code

--- Mastering JavaScript - Fundamentals/Functions Essentials & More/Function-declaration.js
+++ Mastering JavaScript - Fundamentals/Functions Essentials & More/Function-declaration.js
@@ -7,7 +7,16 @@ function computerRectStats(length, width) {
     console.log(`Perimeter: ${perimeterRect}`);
 }
 
+function computePayable(cost) {
+    let discount = cost < 1000 ? 0 : 10;
+    let totalCost = cost - cost * (discount / 100);
+    return totalCost;
+}
+
 let rectALength = 25;
 let rectAWidth = 10;
 
 computerRectStats(rectALength, rectAWidth);
+
+console.log(`The discounted price of the: ${computePayable(2500)}`);
+console.log(`The discounted price of the: ${computePayable(500)}`);
