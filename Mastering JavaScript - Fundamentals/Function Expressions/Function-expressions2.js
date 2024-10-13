8977c69a05aa48efd7a1ea4b2279cda495c06094
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-14 00:44:20 +0530

    convert fehrenhiet to celsius & Celsius to fehrenhiet

---
+++ Mastering JavaScript - Fundamentals/Function Expressions/Function-expressions2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const convertTemp = function(temp, convertTo) {
+    let toUnit = convertTo ?? 'F';
+    let tempToConvert = Number (temp) ?? 0;
+
+    if (toUnit === 'F') {
+        let fahrenheit = (tempToConvert * 9) / 5 + 32;
+        return `${fahrenheit}\u{00B0}F`;
+    } else {
+        let celsius = ((tempToConvert - 32) * 5) / 9;
+        return `${celsius}\u{00B0}C`
+    }
+};
+
+let today = 35;
+console.log(convertTemp(today, 'F'));
+console.log(convertTemp(today, 'C'));
