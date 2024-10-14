39050355e03bdb2bc52e0017bdaad58f59263af2
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-14 23:41:38 +0530

    This keyword function handsOn

---
+++ Mastering JavaScript - Fundamentals/'This' keyword in Function/This-keyword-function-handsOn.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let user = {
+    name: 'Rob Williams',
+    age: 22,
+    livesIn: 'New Jersey',
+    introduction: function() {
+        return `Hi, my name is ${this.name}. I'm ${this.age} years old and I live in ${this.livesIn}`;
+    },
+};
+
+console.log(user.introduction());
