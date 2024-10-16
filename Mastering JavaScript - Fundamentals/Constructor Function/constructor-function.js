028db1a78bb64cc6c946760ea976471bc44de72b
Deepanshu Choudhary <58502400+DeepanshuChoudhary@users.noreply.github.com>
2024-10-16 23:41:31 +0530

    Constructor Function for Name, Agr, isAdmin, canDrive

--- Mastering JavaScript - Fundamentals/Constructor Function/constructor-function.js
+++ Mastering JavaScript - Fundamentals/Constructor Function/constructor-function.js
@@ -6,10 +6,20 @@ function User (name, age, isAdmin) {
     this.name = name;
     this.age = age;
     this.isAdmin = isAdmin;
+    this.canDrive = () => (this.age >= 18 ? 'Yes' : 'No');
+    this.accessAccounts = () => (this.isAdmin ? 'You can Access...' : 'Denied Access...')
 }
 
 const Deepanshu = new User ('Deepanshu Choudhary', 25, true);
 const Anshu = new User ('Anshu Choudhary', 17, false);
 
-console.log(Deepanshu.age);
-console.log(Anshu.age);
+// console.log(Deepanshu.age);
+// console.log(Anshu.age);
+
+// console.log(Deepanshu); - Show all the details of 'Deepanshu'
+
+// console.log(Deepanshu.canDrive());
+// console.log(Anshu.canDrive());
+
+console.log(Deepanshu.accessAccounts());
+console.log(Anshu.accessAccounts());
